8ace0b64b1743693300580fb56f7d228624798da
achraf.bziouech <bziouechachref@gmail.com>
2019-05-19 00:40:38 +0100

    add created by Achraf and push it in Github

--- UserRepository.java
+++ UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.stago.model.User;
 
-
+/**
+Created by achraf
+*/
 public interface UserRepository extends CrudRepository<User, UUID> {
 
 }
